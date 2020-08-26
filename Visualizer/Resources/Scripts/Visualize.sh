6dbdca157405c967da4103ca40309fc6bd005b89
Qub1 <qub1995@gmail.com>
2020-08-26 20:10:37 +0200

    Add visualization script

---
+++ Visualizer/Resources/Scripts/Visualize.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+scriptDirectory="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+projectDirectory="$scriptDirectory/../.."
+resourcesDirectory="$projectDirectory/Resources"
+sourceDirectory="$projectDirectory/Source"
+executable="$sourceDirectory/Visualizer.py"
+interpreter="$projectDirectory/venv/bin/python3"
+database="$projectDirectory/../EnergyManager/Resources/Test Results/database.sqlite"
+
+"$interpreter" "$executable" --database "$database" --output-directory "$resourcesDirectory/Visualizations"
