7a14616ff71cd8dd6fc7fe22cf00da4d8c0ab99d
Qub1 <qub1995@gmail.com>
2020-08-26 18:17:03 +0200

    Update Job script

--- EnergyManager/Resources/Scripts/Job.sh
+++ EnergyManager/Resources/Scripts/Job.sh
@@ -3,12 +3,8 @@
 #SBATCH -N 1
 #SBATCH -t 00:10:00
 
+script="$1"
 scriptDirectory="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-projectDirectory="$scriptDirectory/../.."
-buildDirectory="$projectDirectory/cmake-build"
-resourcesDirectory="$projectDirectory/Resources"
-executable="$buildDirectory/EnergyManager"
-database="$resourcesDirectory/Test Results/database.sqlite"
 
 module purge
 module load 2019
@@ -20,4 +16,4 @@ module load CMake/3.12.1-GCCcore-7.3.0
 cd "$scriptDirectory"
 ./Build.sh
 
-srun "$executable" -d "$database" -t "MatrixMultiplyTest" -p "name=Matrix Multiply Test" -p "cpu=0" -p "gpu=0" -p "matrixAWidth=640" -p "matrixAHeight=640" -p "matrixBWidth=640" -p "matrixBHeight=640"
+srun "$scriptDirectory/$script"
