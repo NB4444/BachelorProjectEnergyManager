0a5fe4bdc2e71f30c0920ca4af999bb19de711ff
Qub1 <qub1995@gmail.com>
2021-01-19 00:09:59 +0100

    Fix incorrect variable name

--- Library/Dependencies/SQLite.cmake
+++ Library/Dependencies/SQLite.cmake
@@ -3,7 +3,7 @@ find_package(SQLite3 REQUIRED)
 
 # Find the include directory
 set(SQLITE_INCLUDE_DIRECTORIES "${SQLite3_INCLUDE_DIRS}")
-message(STATUS "SQLite include directories: ${SQLITE_INCLUDE_DIRECTORY}")
+message(STATUS "SQLite include directories: ${SQLITE_INCLUDE_DIRECTORIES}")
 
 # Find the library
 set(SQLITE_LIBRARIES "${SQLite3_LIBRARIES}")
