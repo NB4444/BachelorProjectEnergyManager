9bb552365d4e89f41d68ebad320552af757752ae
Qub1 <qub1995@gmail.com>
2021-01-20 21:49:50 +0100

    Change build script

--- Visualizer/Build.sh
+++ Visualizer/Build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-"$1" env create --prefix "$2/CondaEnvironment" --file "$3/environment.yml"
-"$2/CondaEnvironment/bin/jupyter" labextension install @jupyter-widgets/jupyterlab-manager
-"$2/CondaEnvironment/bin/jupyter" labextension install @jupyterlab/toc
-"$2/CondaEnvironment/bin/jupyter" labextension install jupyter-matplotlib
+"$1" env create --prefix "$2" --file "$3/environment.yml"
+"$2/bin/jupyter" labextension install @jupyter-widgets/jupyterlab-manager
+"$2/bin/jupyter" labextension install @jupyterlab/toc
+"$2/bin/jupyter" labextension install jupyter-matplotlib
