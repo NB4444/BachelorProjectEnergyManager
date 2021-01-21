68983011b5fdb190badeb672a2bfc1a2b2e415ed
Qub1 <qub1995@gmail.com>
2021-01-21 14:56:35 +0100

    Update visualizer dependencies

--- Visualizer/Build.sh
+++ Visualizer/Build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 "$1" env create --prefix "$2" --file "$3/environment.yml"
-"$2/bin/jupyter" labextension install @jupyter-widgets/jupyterlab-manager
-"$2/bin/jupyter" labextension install @jupyterlab/toc
-"$2/bin/jupyter" labextension install jupyter-matplotlib
+"$2/bin/jupyter" labextension install @jupyter-widgets/jupyterlab-manager @jupyterlab/toc jupyter-matplotlib
