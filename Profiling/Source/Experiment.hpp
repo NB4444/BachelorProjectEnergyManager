fb44746cf83d212c12e3489e77f9ddda1b1c51f0
Nick Breed <nickbreed@outlook.com>
2021-06-12 13:56:23 +0200

    Changed flags to bool

--- Profiling/Source/Experiment.hpp
+++ Profiling/Source/Experiment.hpp
@@ -73,15 +73,16 @@ void experimentEnergyMonitor(const std::map<std::string, std::string>& arguments
 template <class T>
 void experiment(const std::map<std::string, std::string>& arguments, unsigned int iterations) {
 	auto i_max = EnergyManager::Utility::Text::getArgument<unsigned int>(arguments, "-i", 1);
-	bool singleCore = EnergyManager::Utility::Text::getArgument<bool>(arguments, "-s", true);
-	auto policies = EnergyManager::Utility::Text::getArgument<unsigned int>(arguments, "--policies", 0);
-	auto staticFreq = EnergyManager::Utility::Text::getArgument<unsigned int>(arguments, "--staticFreq", 1);
-	
+	bool multicore = EnergyManager::Utility::Text::getArgument<bool>(arguments, "--multicore", false);
+	auto no_policies = EnergyManager::Utility::Text::getArgument<bool>(arguments, "--nopolicies", true);
+	auto staticFreq = EnergyManager::Utility::Text::getArgument<bool>(arguments, "--staticFreq", false);
+	auto singleCore = !multicore;
+
 	for(int i = 0; i < i_max; ++i) {
 		// Control data
 		ENERGY_MANAGER_UTILITY_EXCEPTIONS_EXCEPTION_IGNORE(experimentControl<T>(arguments, iterations));
-		if (staticFreq != 0) {
-			if(policies == 0) {
+		if (!staticFreq) {
+			if(no_policies) {
 				// Energy monitor data
 				ENERGY_MANAGER_UTILITY_EXCEPTIONS_EXCEPTION_IGNORE(experimentEnergyMonitor<T>(arguments, iterations, Policies::Minmax, singleCore));
 				ENERGY_MANAGER_UTILITY_EXCEPTIONS_EXCEPTION_IGNORE(experimentEnergyMonitor<T>(arguments, iterations, Policies::System, singleCore));
